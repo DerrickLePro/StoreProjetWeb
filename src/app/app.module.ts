1992e0334ad45587b9ab669562c16e48fabe6efa
DerrickLePro <kaffoderrick@gmail.com>
2018-04-24 10:39:48 +0100

    organize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,16 +9,28 @@ import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import  "rxjs/add/operator/map";
 import {CategorieService} from "../services/admin/categorie.service";
+import { AboutComponent } from './about/about.component';
+import { ProduitComponent } from './produit/produit.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
 
 const appRoutes:Routes = [
+  {path:'', redirectTo:'/home',pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'product', component:ProduitComponent},
   {path:'category', component:CategorieComponent},
-  {path:'', redirectTo:'#',pathMatch:'full'}
+  {path:'cart', component:CartComponent}
+
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    CategorieComponent
+    CategorieComponent,
+    AboutComponent,
+    ProduitComponent,
+    HomeComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule, RouterModule.forRoot(appRoutes), HttpModule, FormsModule
